35362337356bbe0a2c38ee4f954f4f7a248efa47
tsubamenoki <144215790+tsubamenoki@users.noreply.github.com>
2024-01-06 08:49:34 +0000

    java lesson

---
+++ ScoreArray.java
@@ -0,0 +1,11 @@
+class ScoreArray {
+  public static void main(String []args) {
+    int[] data = { 90, 80, 100, 90 };
+
+    System.out.println("1人目は" + data[0] + "点");
+    System.out.println("2人目は" + data[1] + "点");
+    System.out.println("3人目は" + data[2] + "点");
+    System.out.println("4人目は" + data[3] + "点");
+
+  }
+}
---
+++ ShowData.java
@@ -0,0 +1,15 @@
+class ShowData {
+  public static void main(String []args) {
+    int num = 10;
+    double pi = 3.14;
+    char clood = 'A';
+    boolean fun = true;
+    String msg = "なんとなくおかしいね";
+
+    System.out.println(num);
+    System.out.println(pi);
+    System.out.println(clood);
+    System.out.println(fun);
+    System.out.println(msg);
+  }
+}
