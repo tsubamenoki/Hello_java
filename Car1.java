e957993ba46f9603036752307828da4a9cfc88e4
tsubamenoki <sijiminoki@gmail.com>
2024-01-30 00:18:45 +0900

    finish 3-9

--- Car1.java
+++ Car1.java
@@ -1,24 +1,24 @@
-class Car1 {
-  int no;
-  int speed;
+public class Car1 {
+    int no;
+    int speed;
 
-  void setNo(int n) {
-    no = n;
-  }
+    void setNo(int n){
+        no = n;
+    }
 
-  void run(int s) {
-    speed = s;
-  }
+    void run(int s){
+        speed = s;
+    }
 
-  void brake() {
-    speed = 0;
-  }
+    void brake(){
+        speed = 0;
+    }
 
-  void brake(int s) {
-    speed = speed - s;
-  }
+    void brake(int s){
+        speed = speed - s;
+    }
 
-  void display() {
-    System.out.println("ナンバー" + no + "の速度は" + speed + "です");
-  }
+    void display(){
+        System.out.println("ナンバー" + no + "の速度は" + speed + "です");
+    }
 }
--- DriveCar4.java
+++ DriveCar4.java
@@ -1,10 +1,10 @@
-class DriveCar4 {
-  public static void main(String []args) {
-    Car4 c1 = new Car4();
-    Car4.display();
-    Car4 c2 = new Car4(2525);
-    Car4.display();
-    Car4 c3 = new Car4(8888);
-    Car4.display();
-  }
+public class DriveCar4 {
+    public static void main(String[] args) {
+        Car4 c1 = new Car4();
+        c1.display();
+        Car4 c2 = new Car4(2525);
+        c2.display();
+        Car4 c3 = new Car4(8888);
+        c3.display();
+    }
 }
