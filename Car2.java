dab04f08d4d8c68f1538d0f7b051b6454606b44e
tsubamenoki <sijiminoki@gmail.com>
2024-01-30 23:35:00 +0900

    finish 4-5

--- Car2.java
+++ Car2.java
@@ -1,32 +1,11 @@
 public class Car2 {
-    int no;
-    int speed;
+    private int no;
 
-    Car2(){
-        no = 0;
+    public Car2(int no){
+        this.no = no;
     }
 
-    Car2(int n){
-        no = n;
-    }
-
-    void setNo(int n){
-        no = n;
-    }
-
-    void run(int s){
-        speed = s;
-    }
-
-    void brake(){
-        speed = 0;
-    }
-
-    void brake(int s){
-        speed = speed - s;
-    }
-
-    void display(){
-        System.out.println("ナンバー" + no + "の速度は" + speed + "です");
+    public void display(){
+        System.out.println("ナンバーは" + no + "です");
     }
 }
