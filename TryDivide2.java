aeaef89b1f773f9e63b4e7d569587650a5612afd
tsubamenoki <sijiminoki@gmail.com>
2024-02-19 14:13:21 +0900

    finish 6-4

---
+++ TryDivide2.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class TryDivide2{
+    public static void main(String[] args) {
+        try{
+            int a = Integer.parseInt(args[0]);
+            int b = Integer.parseInt(args[1]);
+            System.out.println("a / b = " + (a / b) + "あまり" + (a % b));
+        }catch(NumberFormatException e){
+            System.out.println("数値を入力してください");
+            e.printStackTrace();
+        }catch(ArithmeticException e){
+            System.out.println("割る数は0以外を入力してください");
+            e.printStackTrace();
+        }catch(Exception e){
+            System.out.println("例外が発生しました");
+            e.printStackTrace();
+        }finally{
+            System.out.println("プログラムを終了します");       
+        }
+    }
+}
